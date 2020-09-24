bca9c06a0cda8e1e3f53fb36fde99d4c3f87bc0e
Shohei Kikuchi <killchance2256@gmail.com>
2020-09-25 02:20:32 +0900

    Add catch all handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const generatePassword = require('password-generator');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,11 @@ app.get('/api/password', (req, res) => {
   const password = generatePassword();
   res.json(password);
   console.log(`Generated a password`);
+})
+
+// For any request that doesn't match one above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 app.listen(PORT, () => {
